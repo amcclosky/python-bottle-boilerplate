47ac86b815e293d8f19b0b4f59f5b0d157a51e7d
Anthony McClosky <anthony@blacklocus.com>
2014-05-03 00:21:33 -0500

    update sever provisioning what have you.

--- .debinate/after_install.sh
+++ .debinate/after_install.sh
@@ -2,5 +2,8 @@
 
 echo 'hello-boilerplate package installed'.
 
+sudo ln -s /etc/nginx/sites-available/hello-conf /etc/nginx/sites-enabled/hello-conf
+rm -rf /etc/nginx/sites-enabled/default
+
 service supervisor restart
 service nginx restart
